07d12612862137f5c66187013802835096555880
kathirm20129 <kathirm20129@gmail.com>
2025-10-03 09:53:33 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -3,8 +3,14 @@
     function add()
     {
       var list=document.createElement("li")
+      list.style.display="inline-block";
+      list.style.backgroundColor="cadetblue";
+      list.style.paddingLeft="10px";
+      list.style.borderRadius="10px";
+      list.style.margin="10px";
       list.innerHTML=inputkey.value+"<button onclick='cancel(event)'>DELETE</button>";
       ul.append(list)
+      inputkey.value='';
     }
     
     function cancel(event)
